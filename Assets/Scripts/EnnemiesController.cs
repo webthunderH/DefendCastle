5eb0e5829e45c5504e1dbca27d23793998ad06b9
ThunderH <henry.dedonder@gmail.com>
2017-05-05 14:56:26 +0200

    add enemy AI
    Enemy controller for waves and life of castle

---
+++ Assets/Scripts/EnnemiesController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiesController : MonoBehaviour {
+    
+    public Transform exitPoint;
+    public float navigationUpdate;
+
+    private GameObject[] checkPoints;
+    private int wave;
+    private Transform ennemy;
+    private float navigationTime;
+
+	// Use this for initialization
+	void Start () {
+        ennemy = GetComponent<Transform>();
+        checkPoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+       
+        
+	}
+    GameObject CheckNextPoint()
+    {
+        GameObject nextPoint=null;
+        float distance = Mathf.Infinity;
+
+        foreach (GameObject a in checkPoints)
+        { 
+            if (a.transform.position != ennemy.position)
+            {
+                Vector3 difference = a.transform.position - ennemy.position;
+                float curDistance = difference.sqrMagnitude;
+                if (curDistance < distance)
+                {
+                    nextPoint = a;
+                    distance = curDistance;
+                }
+            }
+        }
+        return nextPoint;
+    }
+    void ennemyWayToGo()
+    {
+		GameObject nextPoint = CheckNextPoint();
+		if (nextPoint != null)
+		{
+			navigationTime += Time.deltaTime;
+			if (navigationTime > navigationUpdate)
+			{
+                if(nextPoint.name != "End")
+                {
+                    ennemy.position = Vector2.MoveTowards(ennemy.position, nextPoint.transform.position, Time.deltaTime);
+                    //TODO mouvement to the checkpoint
+                   
+                }
+                else
+                {
+                    //TODO before destroy move to;
+                    GameManager.castleLifePoint--;
+                    Destroy(this);
+                }
+
+			}
+		}
+		else
+		{
+
+		}
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+	public GameObject SpawnPoint1,SpawnPoint2,End;
+    public GameObject[] Ennemies;
+    public int maxEnnemiesOnMap,totalEnnemies,ennemiesPerSpawn;
+    public static int castleLifePoint=10;
+   
+    private int ennemiesOnScreen;
+    private int waves = 10;
+    // Use this for initialization
+    private void Awake()
+    {
+        if(instance==null)
+        {
+            instance = this;
+        }
+        else if (instance !=this)
+        {
+            DestroyObject(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start ()
+    {
+        SpawnEnnemies(SpawnPoint1);
+        waves++;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(waves%2==0)
+        {
+            SpawnEnnemies(SpawnPoint1);
+            waves--;
+        }
+        else
+        {
+            SpawnEnnemies(SpawnPoint2);
+            waves--;
+        }
+	}
+
+    void SpawnEnnemies(GameObject Spawn)
+    {
+        if(ennemiesPerSpawn>0 && ennemiesOnScreen<totalEnnemies)
+        {
+            for (int i = 0; i < ennemiesPerSpawn; i++)
+            {
+                if (ennemiesOnScreen<maxEnnemiesOnMap)
+                {
+                    GameObject newEnnemies = Instantiate(Ennemies[0]) as GameObject;
+                    newEnnemies.transform.position = Spawn.transform.position;
+                    ennemiesOnScreen++;
+                }
+            }
+        }
+    }
+
+}
